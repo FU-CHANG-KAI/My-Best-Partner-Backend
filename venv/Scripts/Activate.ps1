28f642ce13503716c7d1cdaa86e9747282f94eeb
ChangKai Fu <53758310+FU-CHANG-KAI@users.noreply.github.com>
2020-02-18 00:37:54 +0000

    add README

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\User\cloud\venv"
+$env:VIRTUAL_ENV="C:\Users\User\My-Best-Partner-Backend\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
